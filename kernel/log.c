8e4d239096002a6240a16730e7f50101aa459b3e
Peter Griggs <petergriggs17@gmail.com>
2019-11-04 21:34:40 -0500

    added comments

--- kernel/log.c
+++ kernel/log.c
@@ -7,31 +7,18 @@
 #include "fs.h"
 #include "buf.h"
 
-// Simple logging that allows concurrent FS system calls.
-//
-// A log transaction contains the updates of multiple FS system
-// calls. The logging system only commits when there are
-// no FS system calls active. Thus there is never
-// any reasoning required about whether a commit might
-// write an uncommitted system call's updates to disk.
-//
-// A system call should call begin_op()/end_op() to mark
-// its start and end. Usually begin_op() just increments
-// the count of in-progress FS system calls and returns.
-// But if it thinks the log is close to running out, it
-// sleeps until the last outstanding end_op() commits.
-//
-// The log is a physical re-do log containing disk blocks.
-// The on-disk log format:
-//   header block, containing block #s for block A, B, C, ...
-//   block A
-//   block B
-//   block C
-//   ...
-// Log appends are synchronous.
 
-// Contents of the header block, used for both the on-disk header block
-// and to keep track in memory of logged block# before commit.
+/*
+In-memory log for EXT3:
+
+Each transaction is an object with the following info:
+  sequence # (what exactly do we do with this?)
+  array of block locations to be logged
+  set of outstanding handles? (ok i have no idea how we use these...)
+  actual log blocks
+*/
+
+
 struct logheader {
   int n;
   int block[LOGSIZE];
