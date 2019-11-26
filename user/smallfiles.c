039f2bf367dfa3e36f79a42943912007fad85668
Kate Thurmer <kthurmer@gmail.com>
2019-11-26 13:57:56 -0500

    updated smallfiles test

--- user/smallfiles.c
+++ user/smallfiles.c
@@ -7,20 +7,47 @@
 #include "kernel/syscall.h"
 #include "kernel/memlayout.h"
 #include "kernel/riscv.h"
+#include "user.h"
 
+//void
+//createWriteSmallFile()
+//{
+//  int i, fd, n;
+//}
 
-void
-createWriteSmallFile()
-{
-  int i, fd, n;
-}
+int main(int argc, char *argv[])  {
 
-int main(int argc, char *argv[])
-{ 
-  char *n = 0;
+  int fd, i, nfiles;
+  char *nraw = 0;
+  char fname;
+
   if(argc > 1) {
-    n = argv[1];
+    nraw = argv[1];
+  } else {
+    printf("Usage: smallfiles <num files>!\n");
+    exit(1);
   }
 
-  
+  nfiles = 0;
+  for (i = 0; nraw[i] != '\0'; i++)
+      nfiles = nfiles * 10 + nraw[i] - '0';
+
+  printf("creating files...\n");
+
+  for (i = 0; i < nfiles; i++) {
+
+    printf("creating a file\n");
+
+    fname = (char) (i + 65);
+    fd = open(&fname, O_CREATE|O_RDWR);
+    if(fd < 0){
+      printf("Error: creat small failed!\n");
+      exit(1);
+    }
+    write(fd, "aaa", 3);
+    close(fd);
+  }
+
+  return 0;
+
 }
